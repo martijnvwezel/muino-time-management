10bbcbbe985fc86f2ce5a64af19f9620d0126345
martijnvwezel <martijnvwezel@muino.nl>
2020-01-21 00:08:35 +0100

    Adding extra documentation

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# muino-time-management
+
+# Muino Time Management
 The Muino Time Management/ accounting tool for keeping track of your hours on projects and tasks.  
+
+# URLs 
+Some list of URLs
+* [Sales webpage for companies](https://sales.muino.nl)
+* [docker hub with latest version](https://hub.docker.com/r/muino/time-management)
+* [telegram](https://t.me/Muinonl)
+
+# Start developing on Muino
+
+## Installation
+``` bash 
+cd muino-angular/
+npm install
+
+cd ../
+
+cd muino-smarthome-api/
+npm install
+
+# change .env to your need
+cp .env.example .env
+
+```
+
+## Developing
+Be aware that you need a MongDB, and Redis version running. Redis will be reconsidered, so it can be removed in the future branches.
+``` bash 
+cd muino-smarthome-api/
+npm run start # This command build the frontend and start the backend
+
+```
+
+## Production
+For the production of pre-build docker on docker-hub.
+``` bash 
+./build_docker.sh
+
+```
---
+++ build_docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# NOT WORKING NO GIT PULL
+git -C muino-angular/  checkout -f master
+git -C muino-angular/  pull origin  master
+
+git -C muino-smarthome-api/  checkout -f master
+git -C muino-smarthome-api/  pull origin master
+
+git submodule update --recursive --remote
+
+echo "$(date)"
+GIT_DIRTY=`git rev-parse HEAD`
+docker login -u "$REGISTRYDOCKER_USER" -p "$REGISTRYDOCKER_PASSWORD" registry.muino.nl/muino-smarthome:latest
+echo "docker pull registry.muino.nl/muino-smarthome:latest || true"
+docker pull registry.muino.nl/muino-smarthome:latest || true
+echo "docker build --cache-from registry.muino.nl/muino-smarthome:latest --tag registry.muino.nl/muino-smarthome:$GIT_DIRTY --tag registry.muino.nl/muino-smarthome:latest ."
+# docker build  --tag registry.muino.nl/muino-smarthome:$GIT_DIRTY --tag registry.muino.nl/muino-smarthome:latest .
+docker build --cache-from registry.muino.nl/muino-smarthome:latest --tag registry.muino.nl/muino-smarthome:$GIT_DIRTY --tag registry.muino.nl/muino-smarthome:latest .
+echo "docker push registry.muino.nl/muino-smarthome:$GIT_DIRTY"
+docker push registry.muino.nl/muino-smarthome:$GIT_DIRTY
+echo "docker push registry.muino.nl/muino-smarthome:latest"
+docker push registry.muino.nl/muino-smarthome:latest
+
+echo "$(date)"
+echo "Wait for 5 sec..."
+sleep 5
+
+ssh server   "./scripts-git/docker/muino-smarthome.sh"
+ssh server   "dm"
