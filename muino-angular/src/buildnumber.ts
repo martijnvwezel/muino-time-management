df19b9033e6252d27a1e1bfd2106242ffd76ecdf
martijnvwezel <martijnvwezel@muino.nl>
2020-01-21 01:09:51 +0100

    Adding correct build number

--- muino-angular/src/buildnumber.ts
+++ muino-angular/src/buildnumber.ts
@@ -1,1 +1,1 @@
-export class BuildNumber {public static readonly number = '2020-01-21T00:08:29Z+3ad6eb1-dirty'}
+export class BuildNumber {public static readonly number = '2020-01-21T00:09:44Z+5fe391c'}
